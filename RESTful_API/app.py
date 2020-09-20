bee380d3bdca94c7752d704415ade108ea370ddc
andre <awjrs22@gmail.com>
2020-09-20 15:19:01 -0400

    updates api with mongodb

--- RESTful_API/app.py
+++ RESTful_API/app.py
@@ -1,9 +1,25 @@
 from flask import Flask, jsonify, request 
 from flask_restful import Api, Resource
-# from pymongo import MongoClient
+from pymongo import MongoClient
 
 app = Flask(__name__)
 api = Api(app)
+
+client = MongoClient("mongodb://db:27017")
+db = client.aNewDB
+UserNum = db["UserNum"]
+
+UserNum.insert({
+    'num_of_users':0
+})
+
+class Visit(Resource):
+    def get(self):
+        prev_num = UserNum.find({})[0]['num_of_users']
+        new_num = prev_num + 1 
+        UserNum.update({}, {"$set":{"num_of_users": new_num}})
+        return str("Hello user " + str(new_num))
+
 
 def checkPostedData(postData, functionName):
     if functionName == "add" or functionName == "subtract" or functionName =='multiply':
@@ -139,6 +155,7 @@ api.add_resource(Add, "/add")
 api.add_resource(Subtract, "/subtract") 
 api.add_resource(Multiply, "/multiply")
 api.add_resource(Divide, "/divide")
+api.add_resource(Visit, "/hello")
 @app.route('/')
 def hello_world():
     return 'Hello Andre'
